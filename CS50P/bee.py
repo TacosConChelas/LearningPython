cfc68b7535aafb0e3afc0c80c10edf300b452605
TacosConChelas <adriherandezor@gmail.com>
2025-10-10 23:50:07 -0600

    se termina le programa bee2, donde se uso .items()

--- CS50P/bee.py
+++ CS50P/bee.py
@@ -1,8 +1,4 @@
-
-from dis import Positions
-
-
-WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
 
 def main():
     print("Welcome to Spelling Bee!")
@@ -12,6 +8,10 @@ def main():
         print(f"{len(WORDS)} words left!")
 
         guess = input("Guess a word: ")
+
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You've won!")
 
         # TODO: check if gess in dictionary
         if guess in WORDS.keys():
