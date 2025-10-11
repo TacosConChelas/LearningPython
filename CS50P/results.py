213433366bdefc42ca0aef178cb7d30f5304f245
TacosConChelas <adriherandezor@gmail.com>
2025-10-11 13:37:27 -0600

    tratado de listas con .append(), .remove(), .insert() y .exted()

---
+++ CS50P/results.py
@@ -0,0 +1,12 @@
+results = ["Mario", "Luigi", "Princess", "Yoshi", "Joopa Troopa", "Toad", "Browser", "Donkey Kong Jr."]
+results.remove("Browser")
+results.insert(0, "Browser")
+
+# results.append(["Browser", "Donkey Kong Jr."])
+# results.remove(["Browser", "Donkey Kong Jr."])
+# results.extend(["Browser", "Donkey Kong Jr."])
+results.reverse()
+
+print(results)
+
+
