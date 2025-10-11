259ee75d7b5613317377a682471829c75cd9dec7
TacosConChelas <adriherandezor@gmail.com>
2025-10-10 23:14:10 -0600

    bee game, uso de .pop() para los diccionarios

---
+++ CS50P/bee.py
@@ -0,0 +1,26 @@
+
+from dis import Positions
+
+
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
+
+def main():
+    print("Welcome to Spelling Bee!")
+    print("Your letters are: A I P C R H G")
+
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} words left!")
+
+        guess = input("Guess a word: ")
+
+        # TODO: check if gess in dictionary
+        if guess in WORDS.keys():
+            points = WORDS.pop(guess)
+            print(f"Good job! You scored {points} points")
+
+
+    print("that's the game!")
+
+
+
+main()
