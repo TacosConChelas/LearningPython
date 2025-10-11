4db0c4cc38a15a389f86f28267fe91a9c14fe386
TacosConChelas <adriherandezor@gmail.com>
2025-10-10 19:54:43 -0600

    Aprendisages de clase

---
+++ mario2.py
@@ -0,0 +1,16 @@
+def main():
+    print_square(3)
+
+
+def print_square(square):
+    for _ in range(square):
+        print("#" * square)
+
+
+def print_row(width):
+    print("#" * width)
+
+
+
+
+main()
