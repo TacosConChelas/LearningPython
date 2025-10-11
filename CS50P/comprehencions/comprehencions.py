f5fcf1c03dbe72185ef60a0541562f7ac787dd81
TacosConChelas <adriherandezor@gmail.com>
2025-10-11 15:01:22 -0600

    llenado de listas y diccionarios

--- CS50P/comprehencions/comprehencions.py
+++ CS50P/comprehencions/comprehencions.py
@@ -5,13 +5,13 @@ def main():
 
     words = get_words("address.txt")
     lowercase_words = [word.lower() for word in words if len(word) > 4]
-    counts = {word: words.count(word for word in words)}
+     
 
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
+    # for word in words:
+        #if word in counts:
+         #   counts[word] += 1
+        #else:
+        #    counts[word] = 1
         
     save_counts(counts)
 
