d2c2c07d9f2bef30340b49bcf0cdd09acbc6be42
TacosConChelas <adriherandezor@gmail.com>
2025-10-26 13:09:49 -0600

    api museo de chicago

--- CS50P/APIs/api.py
+++ CS50P/APIs/api.py
@@ -1,13 +1,18 @@
 import requests
 
 def main():
+    print("Shearch the Art Institute of Chicago!")
+    artist = input("Artist: ")
     try:
-        resultado = requests.get("Https://api.artic.edu/api/v1/artworks/search")
+        resultado = requests.get(
+            "Https://api.artic.edu/api/v1/artworks/search",
+            {"q": artist}
+            )
         for art in resultado.json()["data"]:
             print(f"* {art["title"]}")
     except requests.HTTPError:
         print("Couldn't complete request...")
         return
-        
+
 if __name__ == "__main__":
     main()
