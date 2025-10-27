8eecff223da7a4d1c880aa62a4c62c8e0d3239ae
TacosConChelas <adriherandezor@gmail.com>
2025-10-26 23:08:27 -0600

    Reto 4 completado de listas

--- Concepts/List/sum_of_even_numbers.py
+++ Concepts/List/sum_of_even_numbers.py
@@ -1,3 +1,8 @@
 """
-
+Given a list of numbers, return the sum of all even elements.
 """
+def main():
+    numbers_list = [1, 3, 4, 4, 6, 7, 8, 8, 9, 8]
+    print(sum(numbers_list))    
+if __name__ == "__main__":
+    main()
