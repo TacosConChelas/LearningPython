b6494c3fb974fb695d883cbfe527d74e200d69ca
TacosConChelas <adriherandezor@gmail.com>
2025-10-26 22:40:37 -0600

    Reto 1 completado de listas

---
+++ Concepts/List/remove_duplicates.py
@@ -0,0 +1,13 @@
+"""
+Write a function that removes duplicate elements from a list without using set().
+"""
+def main():
+    numbers_list = [1, 3, 4, 4, 6, 7, 8, 8, 9, 8]
+    for number in numbers_list:
+        count_number = numbers_list.count(number)
+        if count_number > 1:
+            for _ in range(count_number - 1):
+                numbers_list.remove(number)
+    print(numbers_list)
+if __name__ == "__main__":
+    main()
