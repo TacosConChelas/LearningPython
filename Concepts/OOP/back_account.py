95b7c6a99c9ccf7b4ca5e721eece395882cc2d0e
TacosConChelas <adriherandezor@gmail.com>
2025-11-01 22:03:24 -0600

    practicando OOP

---
+++ Concepts/OOP/back_account.py
@@ -0,0 +1,5 @@
+"""
+1 - Create a BankAccount class with _balance, and methods deposit() and withdraw().
+2 - Add a get_balance() method but don’t allow external code to change _balance directly.
+3 - Add a rule: you can’t withdraw if the balance would go below 0.
+"""
