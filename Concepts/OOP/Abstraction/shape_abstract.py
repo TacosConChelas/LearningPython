323201a0905b2f2a079ab209160a8431b4d0faad
TacosConChelas <adriherandezor@gmail.com>
2025-11-03 03:04:08 -0600

    reto terminado

--- Concepts/OOP/Abstraction/shape_abstract.py
+++ Concepts/OOP/Abstraction/shape_abstract.py
@@ -17,7 +17,7 @@ class Shape(ABC):
         pass
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}: Area = {self.area()}, Perimeter = {self.perimeter()}"
+        return f"---- {self.__class__.__name__} ----: Area = {self.area()}, Perimeter = {self.perimeter()}"
 class Circle(Shape):
     def __init__(self, radius : float) -> None:
         super().__init__()
@@ -40,7 +40,26 @@ class Triangle(Shape):
     def perimeter(self) -> float:
         return self._base + self._side_1 + self._side_2
     def __repr__(self) -> str:
-        return super().__repr__() + f"\nBase = {self._base} Side 1 = {self._side_1} Side 2 = {self._side_2}"    
+        return super().__repr__() + f"\nBase = {self._base} Height = {self._height} Side 1 = {self._side_1} Side 2 = {self._side_2}"    
 class Rectangle(Shape):
-    def __init__(self) -> None:
+    def __init__(self, base : float, height : float) -> None:
         super().__init__()
+        self._base = base
+        self._height = height
+    def area(self) -> float:
+        return self._base * self._height
+    def perimeter(self) -> float:
+        return (self._base * 2) + (self._height * 2)
+    def __repr__(self) -> str:
+        return super().__repr__() + f"\nBase = {self._base} Height = {self._height}"
+
+def main():
+    triangle = Triangle(20, 10, 10, 15)
+    circle = Circle(20)
+    rectangle = Rectangle(30, 15)
+
+    for shape in [triangle, circle, rectangle]:
+        print(shape.__repr__())
+if __name__ == "__main__":
+    main()
+
