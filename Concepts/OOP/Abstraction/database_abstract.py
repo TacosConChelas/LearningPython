5538b2bda961ff8fd6764bb18f47627d8d1a92e0
TacosConChelas <adriherandezor@gmail.com>
2025-11-03 15:27:45 -0600

    reto terminado

--- Concepts/OOP/Abstraction/database_abstract.py
+++ Concepts/OOP/Abstraction/database_abstract.py
@@ -22,6 +22,27 @@ class DatabaseConnection(ABC):
     def get_status_conecction(self) -> str:
         return "On" if self._connection else "Off"
     def __repr__(self) -> str:
-        return f"The {self.__class__.__name__}"
+        return (f"<---------Info---------->\n"
+        f"The {self.__class__.__name__}\n"
+        f"Status: {self.get_status_conecction()}")
 class MySQLConnection(DatabaseConnection):
-    
+    def __init__(self, connection: bool) -> None:
+        super().__init__(connection)
+    def connect(self) -> str:
+        return super().connect()
+    def disconnect(self) -> str:
+        return super().disconnect()
+class MongoDBConnection(DatabaseConnection):
+    def __init__(self, connection: bool) -> None:
+        super().__init__(connection)
+    def connect(self) -> str:
+        return super().connect()
+    def disconnect(self) -> str:
+        return super().disconnect()
+def main() -> None:
+    mysql = MySQLConnection(True)
+    mongo = MongoDBConnection(False)
+    print(f"\n".join(f"{db}" for db in [mysql, mongo]))
+
+if __name__ == "__main__":
+    main()
