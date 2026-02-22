826e0b450eb22f8b6f3bf3756178721a18c2d379
TacosConChelas <adriherandezor@gmail.com>
2026-02-22 12:54:04 -0600

    modificaciones

--- Retos/guess_the_word.py
+++ Retos/guess_the_word.py
@@ -4,28 +4,28 @@
 import random
 import re
 def main():
-    word_list = ['candy', 'boy', 'horse', 'toy', 'animal']
+    word_list = ['candy', 'boy', 'horse', 'toy']
     print(f'Those are the word that you have to guess: {word_list}')
     guess_the_word(random.choice(word_list))
     
 def guess_the_word(random_choose="") -> None:
     opptortunities = 0
     letter_matches = ""
-    while opptortunities < len(random_choose):
+    while opptortunities < 5:
         try:
-            #print(random_choose)
-            letter = str(input("Enter the letter: ").lower()).strip()
+            print(random_choose)
+            letter = str(input("Your secret word is like this: " + "_"*len(random_choose) + f"\nEnter the letter, (you only have 5 guesses): ").lower()).strip()
             if letter in random_choose:
-                opptortunities += 1
                 letter_matches += letter
-                print(f"Good job! you need to guess the word in {len(random_choose) - opptortunities } attempts")
-                print(re.sub(f"[^{letter_matches}]", "_", random_choose))
+                new_word = re.sub(f"[^{letter_matches}]", "_", random_choose)
+                if new_word == random_choose:
+                    print('You win'); return
+                print('Good job!', new_word)
             else:
                 print("Try again!")
         except ValueError:
             print("Enter a valid letter")
-    print('You win!')
-
+        opptortunities += 1
 
 if __name__ == "__main__":
     main()
